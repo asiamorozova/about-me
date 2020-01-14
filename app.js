ed7d83ba9c99245b795cd873313c0b36c3a3e92a
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-14 10:03:58 -0800

    test funtion

---
+++ app.js
@@ -0,0 +1,29 @@
+
+//elements from HTML needed to serve as constants 
+const quizButton = document.getElementById('quiz-button');
+const total = document.getElementById('points');
+
+
+// button prompting for the name input
+quizButton.addEventListener('click', () => {
+    console.log ('button works');
+    const name = prompt('What is your name?');
+    const confirmation = confirm(`${name},would you like to take the quiz?`)
+    if(confirmation === false) return;
+
+
+ //question prompts and answer values   
+const answer1 = prompt('Have I been to the place in the photo?'); 
+const answer2 = prompt('Are there three countries listed?');      
+const answer3 = prompt('Are museums listed under number 2?');     
+
+// Points 
+let count = 0;
+if (isYes (answer1)) count += 1;
+if (!isYes (answer2)) count += 1; 
+if (isYes (answer3)) count += 1;
+
+total.textCOntent= '${name}, you got ${count} correct';
+})
+
+//
---
+++ test/tests.js
@@ -0,0 +1,1 @@
+import isYes from './example.test.js';
