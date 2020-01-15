087d274303fb31173b387ba2875ae9b9db8bbe44
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-15 11:37:27 -0800

    final st

--- guessinggame.js
+++ guessinggame.js
@@ -8,7 +8,7 @@ const numberOfTries = document.getElementById('number-of-tries');
 const userGuess = document.getElementById('user-guess');
 const results = document.getElementById('results');
 
-let correctNumber = 9;
+let correctNumber = Math.floor(Math.random() * 20);
 let tries = 3;
 
 guessButton.addEventListener('click', () => {
@@ -19,21 +19,21 @@ guessButton.addEventListener('click', () => {
         results.textContent = 'You are correct!';
         guessButton.disabled = true;
     }
-    if (compareNumbers(guess, correctNumber)===-1) {
-        results.textContent = "Your guess is too low";
+    if (compareNumbers(guess, correctNumber) === -1) {
+        results.textContent = 'Your guess is too low';
 
     }
-    if (compareNumbers(guess, correctNumber)===1) {
-        results.textContent = "Your guess is too high";
+    if (compareNumbers(guess, correctNumber) === 1) {
+        results.textContent = 'Your guess is too high';
     }
-    if (tries === 0 ){
-        numberOfTries.textContent = "You lose. Try again";
-        guessButton.disabled = true ;
+    if (tries === 0) {
+        numberOfTries.textContent = 'You lose. Try again';
+        guessButton.disabled = true;
     }
 
 })
 //guess or user imput 
-const guess = Number(userInput.value); 
+const guess = Number(userInput.value);
 
 //number between 1 and 20
-//var randomNumber = Math.floor(Math.random() *20);
+//var randomNumber = ;
